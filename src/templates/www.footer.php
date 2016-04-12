7037b278acffc0464e75b0c436ea17367a40cc6c
kaestel <martin@think.dk>
2016-04-12 12:38:02 +0200

    Text corrections

--- src/templates/www.footer.php
+++ src/templates/www.footer.php
@@ -14,13 +14,13 @@
 
 	<div id="footer">
 		<div itemtype="http://schema.org/Organization" itemscope class="vcard company">
-			<h2 class="name fn org" itemprop="name">Eiland Hvidevarer A/S, Punkt1 Erhverv</h2>
+			<h2 class="name fn org" itemprop="name">Eiland Hvidevareland A/S, Punkt1 Erhverv</h2>
 
 			<dl class="info basic">
 				<dt class="address">Adresse</dt>
 				<dd class="address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
 					<ul>
-						<li class="streetaddress" itemprop="streetAddress">Stenhusvej 74</li>
+						<li class="streetaddress" itemprop="streetAddress">Stenhusvej 74-78</li>
 						<li class="city"><span class="postal" itemprop="postalCode">4300</span> <span class="locality" itemprop="addressLocality">Holbæk</span></li>
 						<li class="country" itemprop="addressCountry">Denmark</li>
 					</ul>
@@ -33,7 +33,7 @@
 				<dt class="contact">Kontakt os</dt>
 				<dd class="contact">
 					<ul>
-						<li class="email"><a href="mailto:start@think.dk" itemprop="email" content="start@think.dk">start@think.dk</a></li>
+						<li class="email"><a href="mailto:sl@punkt1.dk" itemprop="email" content="sl@punkt1.dk">sl@punkt1.dk</a></li>
 						<li class="tel"><a href="callto:+4570205200" itemprop="tel" content="+4570205200">+45 7020 5200</a></li>
 					</ul>
 				</dd>
